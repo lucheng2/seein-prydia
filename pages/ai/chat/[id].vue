545e897d2c744cce098588477d3998b6c880b437
Ayanami <1694476591@qq.com>
2025-10-12 00:45:35 +0800

    fix: 错误的引用round

--- pages/ai/chat/[id].vue
+++ pages/ai/chat/[id].vue
@@ -927,7 +927,7 @@ const bgRef = ref()
             @scroll="handleScroll"
           >
             <div ref="messagesContainerRef" class="w-full">
-              <AiChatMessage ref="aiChatMessageRef" :chat-type="chatType" :round="currentMessageId" :message-list="messages" />
+              <AiChatMessage ref="aiChatMessageRef" :chat-type="chatType" :round="currentConversationId" :message-list="messages" />
             </div>
           </div>
           <div v-if="chatType === 'bot' || (chatType === 'coach' && !isCoachEnd)" class="shadow-box w-full">
